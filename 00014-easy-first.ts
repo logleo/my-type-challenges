d2e244179a397d5632490d7226d8c438d00fbfc1
logleo <526726697@qq.com>
2023-05-31 11:44:36 +0800

    feat: easy 题解 - 23.5.30

---
+++ 00014-easy-first.ts
@@ -0,0 +1,21 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type cases = [
+  Expect<Equal<First<[3, 2, 1]>, 3>>,
+  Expect<Equal<First<[() => 123, { a: string }]>, () => 123>>,
+  Expect<Equal<First<[]>, never>>,
+  Expect<Equal<First<[undefined]>, undefined>>,
+]
+
+type errors = [
+  // @ts-expect-error
+  First<'notArray'>,
+  // @ts-expect-error
+  First<{ 0: 'arrayLike' }>,
+]
+
+
+// ============= Your Code Here =============
+// 第一项可能是 undefinded（[]），可以考虑用 infer 推断 元组第一项
+type First<T extends any[]> = T extends [infer F, ...any[]] ? F : never;
